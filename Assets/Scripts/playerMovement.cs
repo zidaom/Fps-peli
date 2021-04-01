f348e155938b19c67c52caccf3288325bc25f66c
zidaom <oz6225393@hotmail.com>
2021-04-01 18:12:25 +0300

    taso tehty

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private CharacterController controller;
+    private float gravity = -9.81f;
+
+    private float moveSpeed = 8f, runSpeed = 1.8f;
+    private float pushForce = 5f;
+
+    private float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 4.5f;
+
+    private Vector3 move;
+    private Vector3 velocity;
+
+    public LayerMask groundMask;
+
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        float xAxis = Input.GetAxis("Horizontal");
+        float zAxis = Input.GetAxis("Vertical");
+
+        move = transform.right * xAxis + transform.forward * zAxis;
+
+        if (Input.GetButton("Fire3"))
+        {
+            controller.Move(move * moveSpeed * runSpeed * Time.deltaTime);
+        }
+        else
+        {
+            controller.Move(move * moveSpeed * Time.deltaTime);
+        }
+        
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.collider.gameObject.layer == 6)
+        {
+            return;
+        }
+
+        Rigidbody rb = hit.collider.gameObject.GetComponent<Rigidbody>();
+
+        if (rb == null)
+        {
+            return;
+        }
+
+        rb.AddForce(move * pushForce);
+    }
+}
---
+++ Assets/Scripts/weapon.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+
+    Camera FPSCamera;
+    public GameObject throwablePrefab;
+    private GameObject sphere = null;
+    public float throwForce = 80f;
+    public float maxThrowForce = 4f;
+    private float holdDownTimer;
+
+    // Raycast
+    public float range = 10f;
+    public float pushForce = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPSCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            holdDownTimer = Time.time;
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            float totalTime = Time.time - holdDownTimer;
+            Throw(CalculateHoldDownForce(totalTime * 2f));
+        }
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            ProcessRayCast();
+        }
+    }
+
+    private void ProcessRayCast()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(FPSCamera.transform.position, FPSCamera.transform.forward, out hit, range))
+        {
+            Rigidbody rb = hit.collider.gameObject.GetComponent<Rigidbody>();
+            if (rb == null)
+            {
+                return;
+            }
+
+            rb.AddForce(FPSCamera.transform.forward * pushForce, ForceMode.Impulse);
+        }
+    }
+
+    private void Throw(float throwForce)
+    {
+        if (sphere != null)
+        {
+            Destroy(sphere);
+        }
+
+        sphere = Instantiate(throwablePrefab, transform.position, Quaternion.identity);
+        sphere.GetComponent<Rigidbody>().AddForce(FPSCamera.transform.forward * throwForce, ForceMode.Impulse);
+    }
+
+    private float CalculateHoldDownForce(float holdTime)
+    {
+        float holdTimeNormalized = Mathf.Clamp01(holdTime / maxThrowForce);
+        float force = holdTimeNormalized * throwForce;
+        return force;
+    }
+}
